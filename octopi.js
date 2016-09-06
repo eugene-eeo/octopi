7840ab54d04b9f1d9a232f73733d4232699c905c
Eeo Jun <141bytes@gmail.com>
2016-09-06 09:29:12 +0800

    Revert "serialization/deserialization"

--- octopi.js
+++ octopi.js
@@ -9,11 +9,7 @@ Octopi = function(words) {
 
 Octopi.prototype = {
   constructor: Octopi,
-  /**
-     * Add a new element to the trie
-     * @param key String prefix to look up
-     * @param data Object returned by trie
-     */
+
   add: function(key, data) {
     var id = ++this.uid;
     var sub = this.tree;
@@ -28,10 +24,6 @@ Octopi.prototype = {
     }
   },
 
-  /**
-     * Return the list of elements in the trie for a given query
-     * @param key String The prefix to lookup
-     */
   get: function(key) {
     var sub = this.tree;
     var tbl = this.table;
@@ -42,30 +34,5 @@ Octopi.prototype = {
     return sub.$$.map(function(id) {
       return tbl[id];
     });
-  }
-  /**
-     * Serialize the Octopi trie as string
-     *
-     * @return String
-     */
-  serialize: function(){
-    
-    var o = new Object();
-    o.uid = this.uid;
-    o.tree = this.tree;
-    o.table = this.table;
-
-    return JSON.stringify(o);
-
-  },  
-  /**
-     * Take serialized Octopi data as string and initialize the Octopi object
-     * @param json String The serialized Octopi trie
-     */
-  load: function(json){
-    var o = JSON.parse(json);
-    this.uid = o.uid;
-    this.tree = o.tree;
-    this.table = o.table;
   }
 };
